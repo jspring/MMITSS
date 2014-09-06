c3410bd61ac66c4601b96b0a41d252cb5d92b46d
John Spring <jspring@berkeley.edu>
2014-09-05 17:45:31 -0700

    Added mmitss_msg_hdr_t struct 	Changed port numbers to in and out port numbers

--- src/path/mmitss_ports_and_message_numbers.h
+++ src/path/mmitss_ports_and_message_numbers.h
@@ -1,8 +1,33 @@
-#define PRIO_REQ_PORT           6000
-#define TRAJ_AWARE_PORT         6001
-#define NOMADIC_TRAJ_AWARE_PORT 6002
-#define TRAF_CTL_PORT           6003
-#define TRAF_CTL_IFACE_PORT     6004
-#define SPAT_BCAST_PORT         6005
-#define PERF_OBSRV_PORT         6006
-#define NOMADIC_DEV_PORT        6007
+#pragma	once
+
+#define PRIO_REQ_OUTPORT		6000
+#define PRIO_REQ_INPORT			6001
+#define TRAJ_AWARE_OUTPORT		6002
+#define TRAJ_AWARE_INPORT		6003
+#define NOMADIC_TRAJ_AWARE_OUTPORT	6004
+#define NOMADIC_TRAJ_AWARE_INPORT	6005
+#define TRAF_CTL_OUTPORT		6006
+#define TRAF_CTL_INPORT			6007
+ #define MSG_ID_SIG_PLAN_POLL		'0'
+ #define MSG_ID_SIG_PLAN		0x02
+ #define MSG_ID_SPAT_POLL		'1'
+ #define MSG_ID_SPAT			13
+#define TRAF_CTL_IFACE_OUTPORT		6008
+#define TRAF_CTL_IFACE_INPORT		6009
+#define SPAT_BCAST_OUTPORT		6010
+#define SPAT_BCAST_INPORT		6011
+#define PERF_OBSRV_OUTPORT		6012
+#define PERF_OBSRV_INPORT		6013
+#define NOMADIC_DEV_OUTPORT		6014
+#define NOMADIC_DEV_INPORT		6015
+#define DATA_MANAGER_PORT		6016
+
+// The MMITSS message header convention is:
+//   Header     InternalMsgHeader -- two bytes (0xFFFF)
+//   msgID      MessageID -- one byte(0x01)
+//   timeStamp  CurEpochTime --four bytes (0.01s)
+typedef struct {
+	unsigned short	InternalMsgHeader;
+	unsigned char	msgid;
+	unsigned int	cur_epoch_time;
+} IS_PACKED mmitss_msg_hdr_t;
