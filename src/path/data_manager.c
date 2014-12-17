1f1cf0209e87d4cd149853a09c66708a3b476707
John Spring <jspring@berkeley.edu>
2014-12-16 20:33:04 -0800

    modified:   data_manager.c         Removed cruft #includes at beginning of file         Added forward port for TCI->SPAT_MAP_BCAST_INPORT         Added mapping of inport numbers (known) to fd's (unknown at startup)         Rectified confusion about MMITSS vs. non-MMITSS messages.  The latter         are the polls sent by the TC, et al. for SPaT requests, etc.

--- src/path/data_manager.c
+++ src/path/data_manager.c
@@ -1,4 +1,3 @@
-//#include <db_include.h>
 #include "sys_os.h"
 #include <sys/select.h>
 #include <search.h>
@@ -8,16 +7,13 @@
 #include "msgs.h"
 #include "ab3418commudp.h"
 #include "udp_utils.h"
-#include <asn_codecs.h>
-#include <constr_TYPE.h>
+
+#include "asn_application.h"
+#include "asn_internal.h"       /* for _ASN_DEFAULT_STACK_MAX */
+#include "SPAT.h"
+#include "TimeMark.h"
 
 #define BUFSIZE 1500
-
-/* Convert "Type" defined by -DPDU into "asn_DEF_Type" */
-//#define ASN_DEF_PDU(t)  asn_DEF_ ## t
-//#define DEF_PDU_Type(t) ASN_DEF_PDU(t)
-//#define PDU_Type        DEF_PDU_Type(PDU)
-#define PDU_Type        asn_DEF_SPAT
 
 // "OUT" and "IN" ports are from the perspective of the named 
 // module.  So, for instance, the TrafficControlInterface's output port
@@ -40,8 +36,8 @@ port_t port[] = {
 		{"TrajectoryAwareModule", UDP,  TRAJ_AWARE_OUTPORT, TRAJ_AWARE_INPORT, {0, 0, 0} },
 		{"NomadicTrajectoryAwareModule", UDP,  NOMADIC_TRAJ_AWARE_OUTPORT, NOMADIC_TRAJ_AWARE_INPORT, {0, 0, 0} },
 		{"TrafficControlModule", TCP,  TRAF_CTL_OUTPORT, TRAF_CTL_INPORT, {0, 0, 0} },
-		{"TrafficControlInterfaceModule", TCP,  TRAF_CTL_IFACE_OUTPORT, TRAF_CTL_IFACE_INPORT, {0, 0, 0} },
-		{"SpatBroadcastModule", UDP,  SPAT_BCAST_OUTPORT, SPAT_BCAST_INPORT, {0, 0, 0} },
+		{"TrafficControlInterfaceModule", TCP,  TRAF_CTL_IFACE_OUTPORT, TRAF_CTL_IFACE_INPORT, {SPAT_MAP_BCAST_INPORT, 0, 0} },
+		{"SpatMapBroadcastModule", UDP,  SPAT_MAP_BCAST_OUTPORT, SPAT_MAP_BCAST_INPORT, {0, 0, 0} },
 		{"PerformanceObserverModule", UDP,  PERF_OBSRV_OUTPORT, PERF_OBSRV_INPORT, {0, 0, 0} },
 		{"NomadicDeviceModule", UDP,  NOMADIC_DEV_OUTPORT, NOMADIC_DEV_INPORT, {0, 0, 0} }
 };
@@ -66,12 +62,14 @@ static int write_out(const void *buffer, size_t size, void *key);
 
 int main( int argc, char *argv[]) {
 	int retval;
-	int sockfd[ 2 * NUMPORTS];
-	int newsockfd[ 2 * NUMPORTS];
+	int spatmsgsize;
+	asn_dec_rval_t rval;
+	int sockfd[2 * NUMPORTS];
+	int newsockfd[2 * NUMPORTS];
 	char *local_ip = "127.0.0.1";
 	char *remote_ip = "127.0.0.1";
 	socklen_t localaddrlen = sizeof(struct sockaddr_in);
-	struct sockaddr_in remote_addr[ 2 * NUMPORTS];
+	struct sockaddr_in remote_addr[2 * NUMPORTS];
         int msg_len;
         fd_set readfds, readfds_sav;
         fd_set writefds, writefds_sav;
@@ -88,13 +86,14 @@ int main( int argc, char *argv[]) {
 	int option;
 	int verbose = 0;
 	unsigned short socket_timeout = 10000;       //socket timeout in msec
-	char buf[NUMPORTS][BUFSIZE];
+	unsigned char buf[NUMPORTS][BUFSIZE];
+	unsigned char spatbuf[BUFSIZE];
 	mmitss_msg_hdr_t *mmitss_msg_hdr[NUMPORTS];
 	int i;
 	int j;
 	int k;
 	int l;
-	int m;
+	int m = 0;
 	char tempstr[] = "Got message!";
 	char tempstr2[100];
 	unsigned int ms_since_midnight;
@@ -102,6 +101,8 @@ int main( int argc, char *argv[]) {
 	asn_enc_rval_t erv;
 	static asn_TYPE_descriptor_t PDU_Type;
 	static asn_TYPE_descriptor_t *pduType = &PDU_Type;
+        SPAT_t *spatType_decode=0;
+        spatType_decode = (SPAT_t *) calloc(1, sizeof(SPAT_t));
 
 
 	mmitss_msg_t mmitss_msg[256];
@@ -114,17 +115,20 @@ int main( int argc, char *argv[]) {
 	char *psig_plan_msg = (char *)&sig_plan_msg;
 
 	mmitss_msg[MSG_ID_SPAT_POLL].size = 3;
-	battelle_spat_t *battelle_spat = NULL;
-	mmitss_msg[MSG_ID_SPAT].size = sizeof(battelle_spat_t);
-	mmitss_msg[MSG_ID_SPAT].struct_ptr = (long *)battelle_spat;
-	battelle_spat = calloc(sizeof(battelle_spat_t), 1);
-	char *pbattelle_spat = (char *)&battelle_spat;
+	SPAT_t *j2735_spat = NULL;
+	mmitss_msg[MSG_ID_SPAT].size = sizeof(SPAT_t);
+	mmitss_msg[MSG_ID_SPAT].struct_ptr = (long *)j2735_spat;
+	j2735_spat = calloc(sizeof(SPAT_t), 1);
+	char *pj2735_spat = (char *)&j2735_spat;
 
+   //Need input and output ports
    for(i=0; i< 2 * NUMPORTS; i++) {
 	memset(&sockfd[i], -1, sizeof(sockfd[i]));
 	memset(&newsockfd[i], -1, sizeof(newsockfd[i]));
 	memset(&remote_addr[i], 0, sizeof(remote_addr[i]));
    }
+   
+   //Need only input buffers
    for(i=0; i<NUMPORTS; i++) {
 	mmitss_msg_hdr[i] = (mmitss_msg_hdr_t *)&buf[i][0];
    }
@@ -199,6 +203,8 @@ int main( int argc, char *argv[]) {
 		}
 		else {
 			/** set up remote socket addressing and port */
+			sprintf(tempstr2, "OpenServerListener succeeded for %s output port (%d)", port[i].portdesc, port[i].outport);
+			perror(tempstr2);
 			memset(&remote_addr[i], 0, sizeof(struct sockaddr_in));
 			remote_addr[j].sin_family = AF_INET;
 			remote_addr[j].sin_addr.s_addr = inet_addr(remote_ip);  //htonl(INADDR_ANY);
@@ -216,6 +222,8 @@ int main( int argc, char *argv[]) {
 		}
 		else {
 			/** set up remote socket addressing and port */
+			sprintf(tempstr2, "OpenServerListener succeeded for %s input port (%d)", port[i].portdesc, port[i].inport);
+			perror(tempstr2);
 			memset(&remote_addr[j+1], 0, sizeof(struct sockaddr_in));
 			remote_addr[j+1].sin_family = AF_INET;
 			remote_addr[j+1].sin_addr.s_addr = inet_addr(remote_ip);  //htonl(INADDR_ANY);
@@ -225,6 +233,19 @@ int main( int argc, char *argv[]) {
 				printf("TCP sockfd[%d] %d maxfd %d\n", j+1, sockfd[j+1], maxfd);
 		}
 	}
+    }
+    //Map forwarding file descriptors to nonzero fwdfd's (which were originally set to inport numbers)
+    //File descriptors are only determined after a socket call, so the place of them in the port
+    //struct is initially taken by the (known) number of the destination inport
+    //Here, we replace the destination inport by the destination file descriptor
+    for(i = 0; i < NUMPORTS; i++) {
+	for(j = 0; (j < 3) && (port[i].fwdfd[j] != 0); j++) {
+		for(k = 0; k < NUMPORTS; k++) {
+			if(port[i].fwdfd[j] == port[k].inport) {
+				port[i].fwdfd[j] = newsockfd[k+1];
+    			}
+    		}
+    	}
     }
 
    while(1) {
@@ -236,7 +257,9 @@ int main( int argc, char *argv[]) {
    	for(i = 0; i < NUMPORTS; i++) {
 	    j = 2 * i;
 	    printf("i %d\n", i);
-	    if( (newsockfd[j] <= 0) && (FD_ISSET(sockfd[j], &readfds)) ){
+
+	    //Accept TCP connections if they haven't already been accepted 
+	    if( (port[i].port_type == TCP) && (newsockfd[j] <= 0) && (FD_ISSET(sockfd[j], &readfds)) ){
 		printf("Trying accept for %s output port (%d)\n", port[i].portdesc, port[i].outport);
 		if ((newsockfd[j] = accept(sockfd[j], 
 			(struct sockaddr *) &remote_addr[j], &localaddrlen)) <= 0) {
@@ -250,7 +273,7 @@ int main( int argc, char *argv[]) {
 			FD_SET(newsockfd[j], &readfds_sav);
 			if(newsockfd[j] > maxfd) maxfd = newsockfd[j];
 	   }	
-	    if( (newsockfd[j+1] <= 0) && (FD_ISSET(sockfd[j+1], &writefds)) ){
+	    if( (port[i].port_type == TCP) && (newsockfd[j+1] <= 0) && (FD_ISSET(sockfd[j+1], &writefds)) ){
 		printf("Trying accept for %s input port (%d)\n", port[i].portdesc, port[i].inport);
 		if ((newsockfd[j+1] = accept(sockfd[j+1], 
 			(struct sockaddr *) &remote_addr[j+1], &localaddrlen)) <= 0) {
@@ -264,18 +287,22 @@ int main( int argc, char *argv[]) {
 			FD_SET(newsockfd[j+1], &writefds_sav);
 			if(newsockfd[j+1] > maxfd) maxfd = newsockfd[j+1];
 	   }	
+
+	   //Now try to read client output sockets
 	   if( (newsockfd[j] > 0) && FD_ISSET(newsockfd[j], &readfds) ) {
-		memset(buf[j], 0, BUFSIZE);
+		memset(buf[i], 0, BUFSIZE);
 		printf("Trying read for %s(%d)\n", port[i].portdesc, port[i].outport);
-		if ((retval = read(newsockfd[j], buf[j], BUFSIZE)) <= 0) {
+
+		//If the read fails, close it and reopen it
+		if ((retval = read(newsockfd[j], &buf[i][0], BUFSIZE)) <= 0) {
 		   	sprintf(tempstr2, "read failed for %s(%d)", port[i].portdesc, port[i].outport);
 			perror(tempstr2);
 			FD_CLR(newsockfd[j], &readfds_sav);
 			FD_CLR(newsockfd[j+1], &writefds_sav);
 			close(newsockfd[j]);
-			close(newsockfd[j+1]);
+//			close(newsockfd[j+1]);
 			newsockfd[j] = -1;
-			newsockfd[j+1] = -1;
+//			newsockfd[j+1] = -1;
 
 		    //Error on TCP socket
 		    if(port[i].port_type == TCP) {
@@ -293,20 +320,20 @@ int main( int argc, char *argv[]) {
    				remote_addr[j].sin_port = htons(port[i].outport);
 				FD_SET(sockfd[j], &readfds_sav);
 	    		}
-			sockfd[j+1] = OpenServerListener(local_ip, remote_ip, port[i].inport);
-			if (sockfd[j+1] < 0) {
-		   	    sprintf(tempstr2, "OpenServerListener failed for %s output port (%d)", port[i].portdesc, port[i].inport);
-		   	    perror(tempstr2);
-			    continue;
-			}
-			else {
-   				/** set up remote socket addressing and port */
-   				memset(&remote_addr[j+1], 0, sizeof(struct sockaddr_in));
-   				remote_addr[j+1].sin_family = AF_INET;
-   				remote_addr[j+1].sin_addr.s_addr = inet_addr(remote_ip);  //htonl(INADDR_ANY);
-   				remote_addr[j+1].sin_port = htons(port[i].inport);
-				FD_SET(sockfd[j+1], &writefds_sav);
-	    		}
+//			sockfd[j+1] = OpenServerListener(local_ip, remote_ip, port[i].inport);
+//			if (sockfd[j+1] < 0) {
+//		   	    sprintf(tempstr2, "OpenServerListener failed for %s output port (%d)", port[i].portdesc, port[i].inport);
+//		   	    perror(tempstr2);
+//			    continue;
+//			}
+//			else {
+//   				/** set up remote socket addressing and port */
+//   				memset(&remote_addr[j+1], 0, sizeof(struct sockaddr_in));
+//   				remote_addr[j+1].sin_family = AF_INET;
+//   				remote_addr[j+1].sin_addr.s_addr = inet_addr(remote_ip);  //htonl(INADDR_ANY);
+//   				remote_addr[j+1].sin_port = htons(port[i].inport);
+//				FD_SET(sockfd[j+1], &writefds_sav);
+//	    		}
 	    	    }
 		    else {
 			printf("UDP socket failed\n");
@@ -317,123 +344,139 @@ int main( int argc, char *argv[]) {
 				exit(EXIT_FAILURE);
 			}
 		    }
-	    	}
+	    	}//end of failed read block
         	else {
 			get_current_timestamp(&ts);
 			ms_since_midnight = (3600000 * ts.hour) + (60000 * ts.min) + (1000 * ts.sec) + ts.millisec;
-//			printf("Read succeeded for %s output port (%d) ms-since-midnight %d\n", port[i].portdesc, port[i].outport, ms_since_midnight);
+			printf("Read succeeded for %s output port (%d) ms-since-midnight %d retval %d size %d buf[%d][0] %d m %d mmitss_msg_hdr[i]->msgid %d\n", port[i].portdesc, port[i].outport, ms_since_midnight, retval, mmitss_msg[mmitss_msg_hdr[i]->msgid].size, i, buf[i][0], m++, mmitss_msg_hdr[i]->msgid);
 	
-			//The following looks like magic; it's not. At the beginning of the program, I assigned mmitss_msg_header[i] = &buf[i][0].
-			if(mmitss_msg[mmitss_msg_hdr[i]->msgid].struct_ptr == NULL)
-				mmitss_msg[mmitss_msg_hdr[i]->msgid].struct_ptr = calloc(mmitss_msg[mmitss_msg_hdr[i]->msgid].size, 1);
-//printf("mmitss_msg_hdr[i]->msgid %d mmitss_msg[mmitss_msg_hdr[i]->msgid].size %d\n", mmitss_msg_hdr[i]->msgid, mmitss_msg[mmitss_msg_hdr[i]->msgid].size);
-//			if(retval == mmitss_msg[mmitss_msg_hdr[i]->msgid].size) 
-				{
-//				memcpy(&mmitss_msg[mmitss_msg_hdr[i]->msgid].struct_ptr, &buf[i][j], mmitss_msg[mmitss_msg_hdr[i]->msgid].size);
-				if(mmitss_msg_hdr[i]->InternalMsgHeader == 0xFFFF) {
-				    switch(mmitss_msg_hdr[i]->msgid) {
-					case MSG_ID_SIG_PLAN_POLL:
-	    					if( FD_ISSET(newsockfd[j+1], &writefds)){
-							printf("Trying sig_plan_msg write for %s(%d)\n", port[i].portdesc, port[i].inport);
-							if( (write(newsockfd[j+1], sig_plan_msg, sizeof(sig_plan_msg_t))) != sizeof(sig_plan_msg_t)) {
-						   		sprintf(tempstr2, "sig_plan_msg write failed for %s(%d)", 
-									port[i].portdesc, port[i].inport);
-								perror(tempstr2);
+			//All MMITSS messages start with the InternalMsgHeader=0xFFFF
+			if(mmitss_msg_hdr[i]->InternalMsgHeader == 0xFFFF) {
+				//The following looks like magic; it's not. At the beginning of the program, I assigned mmitss_msg_header[i] = &buf[i][0].
+				if(mmitss_msg[mmitss_msg_hdr[i]->msgid].struct_ptr == NULL)
+					mmitss_msg[mmitss_msg_hdr[i]->msgid].struct_ptr = calloc(mmitss_msg[mmitss_msg_hdr[i]->msgid].size, 1);
+//				printf("mmitss_msg_hdr[i]->msgid %d mmitss_msg[mmitss_msg_hdr[i]->msgid].size %d\n", mmitss_msg_hdr[i]->msgid, mmitss_msg[mmitss_msg_hdr[i]->msgid].size);
+				memcpy(&mmitss_msg[mmitss_msg_hdr[i]->msgid].struct_ptr, &buf[i][0], mmitss_msg[mmitss_msg_hdr[i]->msgid].size);
+
+				switch(mmitss_msg_hdr[i]->msgid) {
+					case MSG_ID_SPAT:
+						switch(buf[i][1+7]) {
+							case 0x82:
+								spatmsgsize = (buf[i][2+7] << 8) + buf[i][3+7] + 4;
+								break;
+							case 0x81:
+								spatmsgsize = buf[i][2+7] + 3;
+								break;
+							case 0x80:
+								spatmsgsize = buf[i][1+7] + 2;
+								break;
+							default:
+								printf("Not a J2735 message (has 0xFFFF header, but not 0x8[012] size\n");
+						}
+						//Copy input buffer to spatbuf
+						memcpy(buf[i], spatbuf, retval);
+
+						//Print out SPaT if desired
+						if(verbose) {
+							spatType_decode=0;
+							rval = ber_decode(0, &asn_DEF_SPAT,(void **)&spatType_decode, &buf[i][0+7], spatmsgsize);
+							xer_fprint(stdout, &asn_DEF_SPAT, spatType_decode);
+							printf("spatType_decode->msgID %d\n", (int)spatType_decode->msgID);
+						}
+
+   						if( FD_ISSET(newsockfd[j+1], &writefds)){
+							printf("Trying j2735_spat write for %s(%d)\n", port[i].portdesc, port[i].inport);
+							if( (write(newsockfd[j+1], j2735_spat, sizeof(SPAT_t))) != sizeof(SPAT_t)) {
+						   		sprintf(tempstr2, "j2735_spat write failed for %s(%d)", 
+								port[i].portdesc, port[i].inport);
+							perror(tempstr2);
 							}
-							if(verbose) {
-								printf("sig_plan_msg send: ");
-								for(k=0; k<sizeof(sig_plan_msg_t); k++)
-			    						printf("%hhx ", psig_plan_msg[k]);
+						if(verbose) {
+							printf("j2735_spat received:\n");
+							for(l=0; l<sizeof(SPAT_t); l+=10){
+							    printf("%d: ", l);
+							    for(m=0; m<10; m++)
+		    						printf("%hhx ", pj2735_spat[l+m]);
 								printf("\n");
 							}
 						}
-						break;
-					case MSG_ID_SPAT_POLL:
-	    					if( FD_ISSET(newsockfd[j+1], &writefds)){
-							printf("Trying battelle_spat write for %s(%d)\n", port[i].portdesc, port[i].inport);
-							if( (write(newsockfd[j+1], battelle_spat, sizeof(battelle_spat_t))) != sizeof(battelle_spat_t)) {
-						   		sprintf(tempstr2, "battelle_spat write failed for %s(%d)", 
-									port[i].portdesc, port[i].inport);
-								perror(tempstr2);
-							}
-							if(verbose) {
-								printf("battelle_spat send:\n");
-								for(l=0; l<sizeof(battelle_spat_t); l+=10){
-								    printf("%d: ", l);
-								    for(m=0; m<10; m++)
-			    						printf("%hhx ", pbattelle_spat[l+m]);
-									printf("\n");
-								}
-							}
+////////////////////////	`		write(STDOUT_FILENO, &j2735_spat, sizeof(SPAT_t));
+//					`	erv = der_encode(pduType, pj2735_spat, write_out, stdout);
+//	`					if(erv.encoded < 0) 
+//		`					fprintf(stderr, ": Cannot convert %s into DER\n",
+//			`				pduType->name);
+//				`			exit(1);
+//					
+//					`	DEBUG("Encoded in %ld bytes of DER", (long)erv.encoded);
 						}
 						break;
-				    }
-				}
-				else {
-				    switch(buf[i][0]) {
-					case MSG_ID_SPAT:
-//	    					if( FD_ISSET(newsockfd[j+1], &writefds)){
-//							printf("Trying battelle_spat write for %s(%d)\n", port[i].portdesc, port[i].inport);
-//							if( (write(newsockfd[j+1], battelle_spat, sizeof(battelle_spat_t))) != sizeof(battelle_spat_t)) {
-//						   		sprintf(tempstr2, "battelle_spat write failed for %s(%d)", 
-//									port[i].portdesc, port[i].inport);
-//								perror(tempstr2);
-//							}
-							if(verbose) {
-								printf("battelle_spat received:\n");
-								for(l=0; l<sizeof(battelle_spat_t); l+=10){
-								    printf("%d: ", l);
-								    for(m=0; m<10; m++)
-			    						printf("%hhx ", pbattelle_spat[l+m]);
-									printf("\n");
-								}
-							}
-////////////////////////							write(STDOUT_FILENO, &battelle_spat, sizeof(battelle_spat_t));
-//							erv = der_encode(pduType, pbattelle_spat, write_out, stdout);
-//							if(erv.encoded < 0) {
-//								fprintf(stderr, ": Cannot convert %s into DER\n",
-//								pduType->name);
-//								exit(1);
-//							}
-//							DEBUG("Encoded in %ld bytes of DER", (long)erv.encoded);
-//						}
-						break;
-				    }
-	    			}
-			}
-//			else {
-//				printf("Message size %d not equal to defined size %d of message number %hhx\n",
-//					retval,
-//					mmitss_msg[mmitss_msg_hdr[i]->msgid].size,
-//					mmitss_msg_hdr[i]->msgid
-//				);
-//			}
-			if(verbose) {
 				for(j=0; j<retval; j++)
 			    		printf("%hhx ", buf[i][j]);
 				printf("\n");
-			}
+			    }//end of reading in MMITSS message
 			
-	  	}
-	  }
-	}
-   	for(i = 0; i < NUMPORTS; i++) {
-	    k = 2 * i;
-	    if( (newsockfd[k+1] >0) && (FD_ISSET(newsockfd[k+1], &writefds)) ){
-		if(port[i].fwdfd[0] != 0) {
-			printf("Trying forwarding write for %s input port (%d)\n", port[i].portdesc, port[i].inport);
-			for(j = 0; (port[i].fwdfd[j] != 0) && j < NUMOUTPORTS; j++) {
-				if( (write(port[i].fwdfd[j], &buf[i][0], retval)) != retval ) {
-		   			sprintf(tempstr2, "forwarding write failed for %s input port (%d)", port[i].portdesc, port[i].inport);
-					perror(tempstr2);
+			//Now try to forward MMITSS message to all appropriate clients
+			if(port[i].fwdfd[0] != 0) {
+				printf("Trying forwarding write for %s input port (%d)\n", port[i].portdesc, port[i].inport);
+				for(k = 0; (port[i].fwdfd[k] != 0) && (k < NUMOUTPORTS); k++) {
+					if( (write(port[i].fwdfd[k], &buf[i][0], retval)) != retval ) {
+		   				sprintf(tempstr2, "forwarding write failed for %s input port (%d)", port[i].portdesc, port[i].inport);
+						perror(tempstr2);
+					}
 				}
-			}
-		}
-	    }
-	}
-    sleep(1);
-    }
-}
+			}//end of forwarding MMITTSS message
+	  	}//end of MMITSS message test
+		else {
+			switch(buf[i][0]) {
+				case MSG_ID_SIG_PLAN_POLL:
+	    				if( FD_ISSET(newsockfd[j+1], &writefds)){
+						printf("Trying sig_plan_msg write for %s(%d)\n", port[i].portdesc, port[i].inport);
+						if( (write(newsockfd[j+1], sig_plan_msg, sizeof(sig_plan_msg_t))) != sizeof(sig_plan_msg_t)) {
+					   		sprintf(tempstr2, "sig_plan_msg write failed for %s(%d)", 
+								port[i].portdesc, port[i].inport);
+							perror(tempstr2);
+						}
+						if(verbose) {
+							printf("sig_plan_msg send: ");
+							for(k=0; k<sizeof(sig_plan_msg_t); k++)
+		    						printf("%hhx ", psig_plan_msg[k]);
+							printf("\n");
+						}
+					}
+					break;
+				case MSG_ID_SPAT_POLL:
+	    				if( FD_ISSET(newsockfd[j+1], &writefds)){
+						printf("Trying j2735_spat write for %s(%d)\n", port[i].portdesc, port[i].inport);
+						if( (write(newsockfd[j+1], j2735_spat, sizeof(SPAT_t))) != sizeof(SPAT_t)) {
+					   		sprintf(tempstr2, "j2735_spat write failed for %s(%d)", 
+								port[i].portdesc, port[i].inport);
+							perror(tempstr2);
+						}
+						if(verbose) {
+							printf("j2735_spat send:\n");
+							for(l=0; l<sizeof(SPAT_t); l+=10){
+							    printf("%d: ", l);
+							    for(m=0; m<10; m++)
+		    						printf("%hhx ", pj2735_spat[l+m]);
+								printf("\n");
+							}
+						}
+					}
+					break;
+				default:
+			    		if(verbose) {
+						printf("Not a known message. Here's the hex string:\n");
+						for(j=0; j<retval; j++)
+							printf("%hhx ", buf[i][j]);
+						printf("Done with unknown message\n\n");
+					}
+			}//end of switch(buf[i][0])
+	  	}//end of non-MMITSS else
+	}//end of successful message read else
+    }//end of attempt to read client output socket
+	}//end of for loop for all defined ports
+    }//end of while loop
+}//end of main
 
 /* Dump the buffer out to the specified FILE */
 static int write_out(const void *buffer, size_t size, void *key) {
